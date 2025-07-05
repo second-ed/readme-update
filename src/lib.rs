2a4aedf9d8143a7b5d50a77eaa47bef4af0c29b7
ed cuss <edcussmusic@gmail.com>
2025-07-05 21:41:31 +0100

    feat: starting to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+use crate::core::{create_file_map, list_files};
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn entry_point(path: String) -> PyResult<()> {
+    dbg!(create_file_map(list_files(path)));
+    Ok(())
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn rs_py_experiment(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(entry_point, m)?)?;
     Ok(())
+}
+
+mod core {
+    use std::{collections::HashMap, ffi::OsStr, fs, path::PathBuf};
+    use walkdir::WalkDir;
+
+    pub fn list_files(path: String) -> Vec<PathBuf> {
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|entry| entry.path().extension() == Some(OsStr::new("py")))
+            .map(|entry| entry.path().to_path_buf())
+            .collect()
+    }
+
+    pub fn create_file_map(paths: Vec<PathBuf>) -> HashMap<PathBuf, String> {
+        paths
+            .into_iter()
+            .filter_map(|path| {
+                fs::read_to_string(&path)
+                    .ok()
+                    .map(|content| (path, content))
+            })
+            .collect()
+    }
 }
