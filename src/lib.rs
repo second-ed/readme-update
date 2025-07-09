afc8755c011db9553464476124780b6c342b54bc
ed cuss <edcussmusic@gmail.com>
2025-07-09 21:43:54 +0100

    feat: main function

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,48 @@
+use std::process::{self, ExitCode};
+
 use pyo3::prelude::*;
 
-use crate::core::{create_readme, extract_docinfo, extract_pyfiles, list_files};
+use crate::core::main;
 
 #[pyfunction]
-fn entry_point(path: String) -> PyResult<()> {
-    let infos = create_readme(extract_docinfo(extract_pyfiles(list_files(path))));
-    println!("{}", infos);
-    Ok(())
+fn py_main(scripts_root: String, readme_path: String) -> PyResult<i8> {
+    match main(scripts_root, readme_path) {
+        ExitCode::SUCCESS => Ok(0),
+        ExitCode::FAILURE => Ok(1),
+        _ => Ok(-1),
+    }
 }
 
 #[pymodule]
 fn rs_py_experiment(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(entry_point, m)?)?;
+    m.add_function(wrap_pyfunction!(py_main, m)?)?;
     Ok(())
 }
 
 mod core {
+    use colored::Colorize;
     use rayon::prelude::*;
     use regex::Regex;
-    use std::{ffi::OsStr, fs, path::PathBuf};
+    use std::process::ExitCode;
+    use std::{ffi::OsStr, fs, io, ops::Deref, path::PathBuf};
     use walkdir::WalkDir;
+
+    pub fn main(scripts_root: String, readme_path: String) -> ExitCode {
+        let paths: Vec<PathBuf> = list_files(scripts_root);
+        // may as well exit early if no readme
+        let readme = ReadMeString::read(&readme_path).expect("Failed to read README.md");
+        let scripts_docs = generate_scripts_docs(paths);
+        let modified_readme = update_readme(&readme, scripts_docs);
+        if modified_readme != readme {
+            modified_readme
+                .write(&readme_path)
+                .expect("Failed to write modified README.md");
+            println!("{}", "Modified README.md".yellow().bold());
+            return ExitCode::FAILURE;
+        }
+        println!("{}", "Nothing to modify".green().bold());
+        ExitCode::SUCCESS
+    }
 
     pub fn list_files(path: String) -> Vec<PathBuf> {
         WalkDir::new(path)
@@ -31,13 +54,13 @@ mod core {
     }
 
     #[derive(Debug)]
-    pub struct PyFile {
+    struct PyFile {
         path: PathBuf,
         code: String,
         docstring: String,
     }
 
-    pub fn extract_pyfiles(paths: Vec<PathBuf>) -> Vec<PyFile> {
+    fn extract_pyfiles(paths: Vec<PathBuf>) -> Vec<PyFile> {
         paths
             .into_par_iter()
             .filter_map(|path| {
@@ -70,11 +93,11 @@ mod core {
     impl DocInfo {
         pub fn to_readme(&self) -> String {
             let basename: String = self.path.file_name().unwrap().to_string_lossy().to_string();
-            format!("| {} | {} | {} |", basename, self.desc, self.link)
+            format!("| `{}` | {} | {} |", basename, self.desc, self.link)
         }
     }
 
-    pub fn extract_docinfo(py_files: Vec<PyFile>) -> Vec<DocInfo> {
+    fn extract_docinfo(py_files: Vec<PyFile>) -> Vec<DocInfo> {
         let mut doc_infos: Vec<DocInfo> = py_files
             .into_par_iter()
             .map(|py_file| {
@@ -101,12 +124,52 @@ mod core {
         doc_infos
     }
 
-    pub fn create_readme(doc_infos: Vec<DocInfo>) -> String {
-        let readme = std::iter::once("# Scripts".to_string())
-            .chain(doc_infos.iter().map(|n| n.to_readme()).collect::<Vec<_>>())
-            .chain(std::iter::once("::".to_string()))
-            .collect::<Vec<_>>()
-            .join("\n");
-        readme
+    fn create_readme(doc_infos: Vec<DocInfo>) -> String {
+        [
+            "# Scripts",
+            "| Name | Description | Link |",
+            "|:---|:---|:---|",
+        ]
+        .into_iter()
+        .map(str::to_string)
+        .chain(doc_infos.iter().map(|n| n.to_readme()).collect::<Vec<_>>())
+        .chain(std::iter::once("::".to_string()))
+        .collect::<Vec<_>>()
+        .join("\n")
+    }
+
+    fn generate_scripts_docs(paths: Vec<PathBuf>) -> String {
+        create_readme(extract_docinfo(extract_pyfiles(paths)))
+    }
+    #[derive(Debug, Eq, PartialEq)]
+    struct ReadMeString(String);
+
+    impl ReadMeString {
+        pub fn read(path: &String) -> Result<Self, io::Error> {
+            fs::read_to_string(path).map(ReadMeString)
+        }
+
+        pub fn write(&self, path: &String) -> Result<(), io::Error> {
+            fs::write(path, &self.0)
+        }
+    }
+
+    impl Deref for ReadMeString {
+        type Target = str;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    fn update_readme(readme: &ReadMeString, scripts_docs: String) -> ReadMeString {
+        let pattern = Regex::new(r"(?s)(?m)^# Scripts.*?^::").expect("valid regex");
+
+        let updated = if pattern.is_match(&readme.0) {
+            pattern.replace(&readme.0, scripts_docs).into_owned()
+        } else {
+            format!("{}\n\n{}", readme.0, scripts_docs)
+        };
+        ReadMeString(updated)
     }
 }
